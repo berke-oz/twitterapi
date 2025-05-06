f07221a3ace793fd383d0176686fb902dd214092
Berke Öz <beerkeozz@outlook.com>
2025-05-07 02:20:02 +0300

    Tweet Response'a yorumlar eklendi.

--- src/main/java/com/twitter/twitterapi/dto/TweetResponse.java
+++ src/main/java/com/twitter/twitterapi/dto/TweetResponse.java
@@ -1,6 +1,7 @@
 package com.twitter.twitterapi.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record TweetResponse(Long id, String content, LocalDateTime dateTime, String userName) {
+public record TweetResponse(Long id, String content, LocalDateTime dateTime, String userName, List<CommentResponse> comments) {
 }
