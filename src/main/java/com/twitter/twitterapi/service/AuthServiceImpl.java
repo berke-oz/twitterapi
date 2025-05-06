ea0dff8af42fec3072cc887e3e10e447d1e72bfd
Berke Öz <beerkeozz@outlook.com>
2025-05-07 01:11:19 +0300

    Ana Exception ApiException olarak güncellendi.

--- src/main/java/com/twitter/twitterapi/service/AuthServiceImpl.java
+++ src/main/java/com/twitter/twitterapi/service/AuthServiceImpl.java
@@ -1,36 +1,38 @@
-package com.twitter.twitterapi.service;
+    package com.twitter.twitterapi.service;
 
-import com.twitter.twitterapi.entity.User;
-import com.twitter.twitterapi.exceptions.UserException;
-import com.twitter.twitterapi.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+    import com.twitter.twitterapi.entity.User;
+    import com.twitter.twitterapi.exceptions.ApiException;
+    import com.twitter.twitterapi.repository.UserRepository;
+    import lombok.AllArgsConstructor;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
-@Service
-public class AuthServiceImpl implements AuthService{
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @AllArgsConstructor
+    @Service
+    public class AuthServiceImpl implements AuthService{
 
 
-    @Override
-    public String login(String email, String password) {
-        User user = userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new UserException("User not found with email: " + email, HttpStatus.NOT_FOUND));
+        private final UserRepository userRepository;
+        private final PasswordEncoder passwordEncoder;
 
-        if (!passwordEncoder.matches(password, user.getPassword())) {
-            throw new UserException("Invalid password", HttpStatus.UNAUTHORIZED);
+
+        @Override
+        public String login(String email, String password) {
+            User user = userRepository.findUserByEmail(email)
+                    .orElseThrow(() -> new ApiException("User not found with email: " + email, HttpStatus.NOT_FOUND));
+
+            if (!passwordEncoder.matches(password, user.getPassword())) {
+                throw new ApiException("Invalid password", HttpStatus.UNAUTHORIZED);
+            }
+
+            return "Login successful";
         }
 
-        return "Login successful";
-    }
 
-    @Override
-    public User register(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+            @Override
+            public User register(User user) {
+                user.setPassword(passwordEncoder.encode(user.getPassword()));
+                return userRepository.save(user);
+            }
     }
-}
