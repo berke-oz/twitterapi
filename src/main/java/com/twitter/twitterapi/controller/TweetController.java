9a09d6d200af5a99fb25314f52dc30983aedcc98
Berke Öz <beerkeozz@outlook.com>
2025-05-07 01:05:05 +0300

    Tweet Controller oluşturuldu.

---
+++ src/main/java/com/twitter/twitterapi/controller/TweetController.java
@@ -0,0 +1,61 @@
+package com.twitter.twitterapi.controller;
+
+import com.twitter.twitterapi.dto.TweetRequest;
+import com.twitter.twitterapi.dto.TweetResponse;
+import com.twitter.twitterapi.service.TweetService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/tweet")
+public class TweetController {
+
+    private final TweetService tweetService;
+
+
+    @PostMapping()
+    public ResponseEntity<TweetResponse> createTweet(@RequestBody TweetRequest tweetRequest, Authentication authentication){
+        String currentUserEmail = authentication.getName();
+        TweetResponse tweetResponse = tweetService.createTweet(tweetRequest, currentUserEmail);
+        return new ResponseEntity<>(tweetResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<TweetResponse>> getTweetsByUserId(@PathVariable Long userId){
+        List<TweetResponse> tweets = tweetService.getTweetsByUserId(userId);
+        return new ResponseEntity<>(tweets, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TweetResponse> getTweetById(@PathVariable Long id){
+        TweetResponse tweet = tweetService.getTweetById(id);
+        return new ResponseEntity<>(tweet, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<TweetResponse> updateTweetById(@PathVariable Long id, @RequestBody TweetRequest tweetRequest, Authentication authentication ){
+        String currentUserEmail = authentication.getName();
+        TweetResponse updatedTweet = tweetService.updateTweet(id, tweetRequest, currentUserEmail);
+
+        return new ResponseEntity<>(updatedTweet, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable Long id, Authentication authentication) {
+
+        String currentUserEmail = authentication.getName();
+
+        tweetService.deleteTweet(id, currentUserEmail);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
