919bc9c6464b8064f7e1fa0487ba75c793535359
Berke Öz <beerkeozz@outlook.com>
2025-05-12 00:42:54 +0300

    Update UserRequest.java

--- src/main/java/com/twitter/twitterapi/dto/UserRequest.java
+++ src/main/java/com/twitter/twitterapi/dto/UserRequest.java
@@ -2,6 +2,9 @@ package com.twitter.twitterapi.dto;
 
 import com.twitter.twitterapi.entity.Tweet;
 import com.twitter.twitterapi.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
@@ -9,10 +12,22 @@ import java.util.List;
 @Data
 public class UserRequest {
 
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 15, message = "Username must be between 3 and 15 characters")
     private String userName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
 
 
