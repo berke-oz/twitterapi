df14a3d640201506afe59023500f53441fabd14c
Berke Öz <beerkeozz@outlook.com>
2025-05-12 00:39:47 +0300

    Retweet Entitys için Request ve Response oluşturuldu.

---
+++ src/main/java/com/twitter/twitterapi/dto/RetweetRequest.java
@@ -0,0 +1,10 @@
+package com.twitter.twitterapi.dto;
+
+import lombok.Data;
+
+@Data
+public class RetweetRequest {
+    private Long tweetId;
+
+
+}
---
+++ src/main/java/com/twitter/twitterapi/dto/RetweetResponse.java
@@ -0,0 +1,5 @@
+package com.twitter.twitterapi.dto;
+
+public record RetweetResponse(Long id, Long tweetId, String userName, String tweetContent) {
+
+}
